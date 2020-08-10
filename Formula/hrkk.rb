148d70b55c1faa437202d39ca29f2f464308064d
K2Da <id.k2da@gmail.com>
2020-08-10 09:06:58 +0900

    hrkk v0.2.1

--- Formula/hrkk.rb
+++ Formula/hrkk.rb
@@ -1,16 +1,16 @@
 class Hrkk < Formula
   desc "List up aws resources and select some with fuzzy finder"
   homepage "https://github.com/K2Da/hrkk"
-  version "v0.1.1"
-  v = "v0.1.1"
+  version "v0.2.1"
+  v = "v0.2.1"
 
   base_url = "https://github.com/K2Da/hrkk/releases/download"
   if OS.linux?
-    url "#{base_url}/#{v}/hrkk-#{v}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "45f4127de2f39c1cd085416d7881b0b076f30f4e69459a312c5fd5915cd7e9cd"
+    url "#{base_url}/#{v}/hrkk-#{v}-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "b9b7043191fd76bedaa3dbaf105c5c91f30520bace5e6f98a4db3e42781ba311"
   elsif OS.mac?
     url "#{base_url}/#{v}/hrkk-#{v}-x86_64-apple-darwin.tar.gz"
-    sha256 "1959b864169f71b894f5a11155dfc1aa817b6ab806be2242ed8ebe43a99cd4c9"
+    sha256 "6406c3b0032adbac4b0eb11c3a29b9c8a3c307f3b5cad78762deea9df802bc37"
   end
 
   def install
