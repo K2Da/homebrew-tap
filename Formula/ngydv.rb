0cd30c6aa4a0935a613e86e468572e6ea24b8848
K2Da <id.k2da@gmail.com>
2020-08-30 15:51:28 +0900

    ngydv v0.1.5

--- Formula/ngydv.rb
+++ Formula/ngydv.rb
@@ -1,17 +1,17 @@
 class Ngydv < Formula
   desc "Create aws cli session keys with mfa-token and store, export it"
   homepage "https://github.com/K2Da/ngydv"
-  version "v0.1.4"
-  v = "v0.1.4"
+  version "v0.1.5"
+  v = "v0.1.5"
 
   base_url = "https://github.com/K2Da/ngydv/releases/download"
 
   if OS.linux?
     url "#{base_url}/#{v}/ngydv-#{v}-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "dbbd6b462d2382d55c9c26fdaeb8e477c02132fedba80c2e7547486e7b35b13c"
+    sha256 "343a337851964dbedafa2a15b95a7c6c9a013b571b662662ad7d5ac02e3013ee"
   elsif OS.mac?
     url "#{base_url}/#{v}/ngydv-#{v}-x86_64-apple-darwin.tar.gz"
-    sha256 "30f88e941361a74a7dd62a335f8d6cf49c2c60a9ba92d6578926fd8b84d00caa"
+    sha256 "c5eacdbcf7d6ec5d34023a9d14f6455272e8b03e14d5458d3eb2f1ffe2c7ff90"
   end
 
   def install
